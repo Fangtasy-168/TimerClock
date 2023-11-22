d35d04443537a98a04dc7dafef9214bfc5b209d0
Tony Fang <tonyfang1897@gmail.com>
2023-11-21 21:06:22 -0500

    Vanilla JS of Timer Clock

--- App/JS/TimerClock.js
+++ App/JS/TimerClock.js
@@ -1,49 +1,101 @@
 // Vanilla Javascript Timer Clock with Break Intervals
 let rest = document.querySelector("#break-length")
 let session = document.querySelector("#session-length")
-let started = false
-
+let timeLeft = document.querySelector("#time-left")
 let start = document.querySelector("#start_stop")
 let crement = document.querySelectorAll(".modify")
+let reset = document.querySelector("#reset")
+let started = false
+let timerLabel = document.querySelector("#timer-label")
+let sessionIncrement = document.querySelector("#session-increment")
+let sessionDecrement = document.querySelector("#session-decrement")
+let breakIncrement = document.querySelector("#break-increment")
+let breakDecrement = document.querySelector("#break-decrement")
+let beep = document.querySelector("#beep")
 
-//Event Listners
-start.addEventListener("click", ticking)
-
-crement.forEach((crement) => {
-    crement.addEventListener("click", upDown)
+//Event Listeners
+document.addEventListener("DOMContentLoaded", () => {
+    timeDisplay()
 })
 
+start.addEventListener("click", () => {
+    started == true ? started = false : started = true
+    ticking()
+})
 
-//Functions for the event listners
-function upDown(e) {
-    if (!started) {
-        if (e.target.classList.contains("fa-chevron-up")) {
-            e.currentTarget.previousElementSibling.textContent = Number(e.currentTarget.previousElementSibling.textContent) + 1
-        }
-        else if (e.currentTarget.nextElementSibling.textContent > 1) {
-            e.currentTarget.nextElementSibling.textContent = Number(e.currentTarget.nextElementSibling.textContent) - 1
-        }
+sessionIncrement.addEventListener("click", testupDown)
+sessionDecrement.addEventListener("click", testupDown)
+breakIncrement.addEventListener("click", testupDown)
+breakDecrement.addEventListener("click", testupDown)
+
+
+function testupDown(e) {
+    if (e.currentTarget.getAttribute("id").includes("decrement") && e.currentTarget.nextElementSibling.textContent > 1) {
+        e.currentTarget.nextElementSibling.textContent = Number(e.currentTarget.nextElementSibling.textContent) - 1
     }
+    else if (e.currentTarget.getAttribute("id").includes("increment") && e.currentTarget.previousElementSibling.textContent < 60) {
+        e.currentTarget.previousElementSibling.textContent = Number(e.currentTarget.previousElementSibling.textContent) + 1
+    }
+    timeRemaining = Number(session.textContent) * 60000
+    timeDisplay()
 }
 
+reset.addEventListener("click", () => {
+    clearInterval(nIntervId)
+    started = false
+    cycle = session
+    rest.textContent = 5
+    session.textContent = 25
+    timerLabel.textContent = "Session"
+    timeRemaining = Number(session.textContent) * 60000
+    timeDisplay()
+    beep.pause()
+    beep.currentTime = 0
+})
 
 let nIntervId
+let cycle = session
+let timeRemaining = Number(session.textContent) * 60000
+
 //Timer Function
 function ticking() {
-    started == true ? started = false : started = true
+    let goalTime
     if (started) {
-        let endTime = new Date().setMinutes(new Date().getMinutes() + Number(session.textContent))
-        let currentDate = new Date()
+        timeRemaining > 1000 ? goalTime = timeRemaining : goalTime = (Number(cycle.textContent) * 60000)
+        let endTime = Date.now() + goalTime + 1000
         nIntervId = setInterval(() => {
             let updateDate = new Date()
-            let timeRemaining = endTime - updateDate
-            if (timeRemaining < 1) {
-                endTime = new Date().setMinutes(new Date().getMinutes() + Number(rest.textContent))
-                currentDate = new Date()
+            timeRemaining = endTime - updateDate
+            if (timeRemaining < 1000) {
+                timeRemaining = 0
+                alarm()
+                timeDisplay()
+                clearInterval(nIntervId)
+
+                setTimeout(() => {
+                    cycle = cycle == session ? rest : session
+                    ticking()
+                }, 1000)
+            } else {
+                timeDisplay()
             }
-            console.log(timeRemaining / 1000)
-        }, 1000)
+        }, 500)
+
     } else {
         clearInterval(nIntervId)
     }
+}
+function alarm() {
+    beep.play()
+    console.log("times up")
+}
+
+function timeDisplay() {
+    timerLabel.textContent = cycle == session ? "Session" : "Break"
+    let min = Math.floor(timeRemaining / 1000 / 60)
+    let sec = Math.floor(timeRemaining / 1000 % 60)
+    let display = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`
+    timeLeft.textContent = display
+    console.log(cycle.getAttribute("id"))
+    console.log(display)
 }
